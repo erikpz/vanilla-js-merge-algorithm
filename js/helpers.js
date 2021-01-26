6e8e93e03568d76bea7b1a9a9d5ef6a681869d0a
Erik Pizana <erikpzcanedo@outlook.com>
2021-01-25 23:19:21 -0600

    Images added,

--- js/helpers.js
+++ js/helpers.js
@@ -9,13 +9,9 @@ const convertInput = (str) => {
         strn = ''
     }
     return strn
-<<<<<<< HEAD
 }
 
 const PASO1 = 'Se parte de tener un arreglo de tamaño n desordenado. En caso de que la longitud sea mayor a 1 se divide todo el arreglo en dos partes aproximadamente iguales.';
 const PASO2 = 'Se dividen los subarreglos resultantes hasta que los arreglos sean de tamaño uno.';
 const PASO3 = 'Posteriormente se comparan los elementos de cada lista y se combinan de una manera ordenada en un solo arreglo. Al tener los subarreglos acomodados únicamente se comparan los extremos de cada arreglo, ya que contienen los valores más pequeños.';
 const PASO4 = 'Se combinan los subarrelglos ordenados en un solo arreglo, obteniendo el arreglo original acomodado.';
-=======
-}
->>>>>>> origin/historia
